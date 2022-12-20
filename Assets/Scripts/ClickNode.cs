9e5898b073a2e55a8ab922ec8ae2d2fc5b6b2269
woo <coinus@naver.com>
2022-12-20 16:57:10 +0900

    AddAsset
    asset ADd

--- Assets/Scripts/ClickNode.cs
+++ Assets/Scripts/ClickNode.cs
@@ -6,22 +6,19 @@ public class ClickNode : MonoBehaviour
 {
     [SerializeField] GameObject[] drawObj;
     int ranNum;
-    private void Start()
-    {
- 
-    }
 
 
     private void Update()
     {
+        
        
         if (Input.GetMouseButtonDown(0))
         {
-            //ranNum = Random.Range(0,ra)
+            ranNum = Random.Range(0, drawObj.Length);
 
             Vector2 mPosition = Input.mousePosition;
             Vector2 target = Camera.main.ScreenToWorldPoint(mPosition);
-           // Instantiate(drawObj, target, Quaternion.identity);
+            Instantiate(drawObj[ranNum], target, Quaternion.identity);
         }
     }
 }
