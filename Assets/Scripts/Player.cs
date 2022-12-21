eb6b01688c0af7e619b7b7f19145fbfd2de011be
woo <coinus@naver.com>
2022-12-21 10:38:34 +0900

    BackAdd

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+    float limitLeft = -1.8f;
+    float limitRight = 1.8f;
 
-    // Update is called once per frame
+
     void Update()
     {
         Vector2 mPosition = Input.mousePosition;
-
         Vector2 target = Camera.main.ScreenToWorldPoint(mPosition);
 
-        this.transform.position = new Vector2(0, mPosition.y);
+
+        Debug.Log(mPosition);
+
+        this.transform.position = new Vector2(target.x, 4);
+
+        if(transform.position.x <= limitLeft)
+        {
+
+        }
+
+      
     }
 }
