1b1939e34dffef2d2d646f261f762b2b04f70fc8
woo <coinus@naver.com>
2022-12-21 11:33:51 +0900

    Test
    point

--- Assets/Scripts/ClickNode.cs
+++ Assets/Scripts/ClickNode.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 
 public class ClickNode : MonoBehaviour
 {
+    Player player;
+
     [SerializeField] GameObject[] drawObj;
+
     Vector2 limit = new Vector2(0, 4f);
     int ranNum;
+
     private void Start()
     {
+       // player = GetComponent<Player>();
+        ranNum = Random.Range(0, drawObj.Length);
+        Instantiate(drawObj[ranNum], limit, Quaternion.identity);
+
+        player = FindObjectOfType<Player>();
+    }
+
+    private void Update()
+    {
+        if(player.flag == true)
+        {
+            StartCoroutine(NextDrawObj());
+        }
+        
+    }
+
+    IEnumerator NextDrawObj()
+    {
+        yield return new WaitForSeconds(1.5f);
         ranNum = Random.Range(0, drawObj.Length);
         Instantiate(drawObj[ranNum], limit, Quaternion.identity);
     }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    float limitLeft = -1.8f;
-    float limitRight = 1.8f;
+    Rigidbody2D rb;
+
+    float limitLeft = -2.45f;
+    float limitRight = 2.45f;
+    float firstY = 3.8f;
+    public bool flag = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.Sleep();
+    }
 
 
     void Update()
@@ -13,16 +23,28 @@ public class Player : MonoBehaviour
         Vector2 mPosition = Input.mousePosition;
         Vector2 target = Camera.main.ScreenToWorldPoint(mPosition);
 
-
-        Debug.Log(mPosition);
-
-        this.transform.position = new Vector2(target.x, 4);
-
-        if(transform.position.x <= limitLeft)
+        if (flag == false)
         {
+            this.transform.position = new Vector2(target.x, 4);
+            if (transform.position.y >= firstY)
+            {
+                if (transform.position.x <= limitLeft)
+                {
+                    transform.position = new Vector2(-2.45f, 4);
+                }
+
+                if (transform.position.x >= limitRight)
+                {
+                    transform.position = new Vector2(2.45f, 4);
+                }
+            }
 
         }
 
-      
+        if (Input.GetMouseButtonDown(0))
+        {
+            flag = true;
+            rb.WakeUp();
+        }
     }
 }
