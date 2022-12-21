9a1fdfcdd6da48d5d2e99bd9a027569dc737a11d
woo <coinus@naver.com>
2022-12-21 15:04:53 +0900

    CombineObjNum
    조합시 넘버 더해주면 다음 쿠키 등장

--- Assets/Scripts/ClickNode.cs
+++ Assets/Scripts/ClickNode.cs
@@ -5,36 +5,74 @@ using UnityEngine;
 public class ClickNode : MonoBehaviour
 {
     [SerializeField] GameObject[] drawObj;
-
     Vector2 limit = new Vector2(0, 4f);
-    int ranNum;
     bool nodeFlag = true;
-    float timer;
+    bool cookie3 = false;
+    bool cookie4 = false;
+    bool cookie5 = false;
+    bool cookie6 = false;
+    bool cookie7 = false;
+    bool cookie8 = false;
+    int ranNum;
+    public int combineCheck = 2;
+
     private void Start()
     {
-        ranNum = Random.Range(0, drawObj.Length);
+        ranNum = Random.Range(0, combineCheck);
         Instantiate(drawObj[ranNum], limit, Quaternion.identity);
     }
 
     private void Update()
     {
-        timer += Time.deltaTime;
-
         if (Input.GetMouseButtonDown(0) && nodeFlag == true)
         {
             nodeFlag = false;
 
             if (nodeFlag == false)
-            StartCoroutine(NextDrawObj());
+                StartCoroutine(NextDropObj());
         }
+        NextCombine();
     }
 
-    IEnumerator NextDrawObj()
+
+    void NextCombine()
+    {
+
+        if (cookie3 == true && combineCheck < 2)
+        {
+            combineCheck += 1;
+        }
+        if (cookie4 == true && combineCheck < 3)
+        {
+            combineCheck += 1;
+        }
+        if (cookie5 == true && combineCheck < 4)
+        {
+            combineCheck += 1;
+        }
+        if (cookie6 == true && combineCheck < 5)
+        {
+            combineCheck += 1;
+        }
+        if (cookie7 == true && combineCheck < 6)
+        {
+            combineCheck += 1;
+        }
+        if (cookie8 == true && combineCheck < 7)
+        {
+            combineCheck += 1;
+        }
+        if (cookie8 == true && combineCheck < 8)
+        {
+            combineCheck += 1;
+        }
+    }
+    IEnumerator NextDropObj()
     {
         yield return new WaitForSeconds(1.5f);
-        ranNum = Random.Range(0, drawObj.Length);
+        
+        ranNum = Random.Range(0, combineCheck);
         Instantiate(drawObj[ranNum], limit, Quaternion.identity);
         nodeFlag = true;
     }
-
 }
